0c94bc27c8c611f897870b302c9871f4d77fffc5
SnowyStarfall <65053311+SnowyStarfall@users.noreply.github.com>
2020-12-17 15:50:19 -0600

    Made the bot work THANK GOD

--- main.js
+++ main.js
@@ -12,31 +12,32 @@ logger.level = 'debug';
 var bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 bot.username = 'UnbiddenBot';
-bot.id = 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info(`Logged in as: ${bot.user.tag}!`);
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.id =
+    bot.on('ready', function (evt) {
+        logger.info('Connected');
+        logger.info(`Logged in as: ${bot.user.tag}!`);
+        logger.info(bot.username + ' - (' + bot.id + ')');
+    });
+bot.on('message', function (message) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    console.log(user, userID, channelID, message, evt);
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+
+    const user = message.author,
+          userID = user.id,
+          channelID = message.channel.id,
+          content = message.content;
+    console.log(user, userID, channelID, content);
+    if (content.substring(0, 1) == '!') {
+        var args = content.substring(1).split(' ');
         var cmd = args[0];
-       
         args = args.splice(1);
-        switch(cmd) {
+        switch (cmd) {
             // !ping
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
+                message.reply('Pong!')
+                break;
             // Just add any case commands if you want to..
-         }
-     }
+        }
+    }
 });
 bot.login(TOKEN);

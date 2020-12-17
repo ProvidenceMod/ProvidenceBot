859401791bf15d313189f797ac92dfd5ec515f32
SnowyStarfall <65053311+SnowyStarfall@users.noreply.github.com>
2020-12-17 15:32:05 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+var Discord = require('discord.js');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+bot.username = 'UnbiddenBot';
+bot.id = 
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info(`Logged in as: ${bot.user.tag}!`);
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    console.log(user, userID, channelID, message, evt);
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
+bot.login(TOKEN);

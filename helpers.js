f637b12144f06cf874eca8e4c735ce3509c6b8de
Zack <zackhemmingwayfox@gmail.com>
2021-02-06 01:20:27 -0600

    Variable wrapping for suggest

--- helpers.js
+++ helpers.js
@@ -9,24 +9,57 @@ const pluckFirstQuotedString = function(string) {
   return `${string.split('"')[1]}`;
 };
 
-const readChangelog = function() {
-  let d = fs.readFileSync('./changelogdata.json');
+const splitWrapped = function(string, wrappableIndicator, includeWrappings = false) {
+  // Preliminary check if it's a valid indicator
+  if (wrappableIndicator !==  '{}' && wrappableIndicator !==  '[]' && wrappableIndicator !== '()') return null;
+  // Definitions
+  const split = [];
+  let open = false;
+  let s = ``;
+  // Loop through string, with preliminary shorthand assign
+  for (let i = 0; i < string.length; i++) {
+    const c = string[i];
+    // If it's an opening wrap, start including
+    if (c === wrappableIndicator[0]) {
+      open = true;
+      s += c;
+      continue;
+    }
+    // If open, include everything
+    if (open) {
+      s += c;
+      // If what we just included is a closer, push it up, reset and move on
+      if (c === wrappableIndicator[1]) {
+        open = false;
+        split.push(s);
+        s = ``;
+        continue;
+      }
+    }
+  }
+  if (!includeWrappings) split.forEach((section, i) => { split[i] = section.slice(1, section.length - 1); });
+
+  return split;
+};
+
+const readChangelog = function(debug = false) {
+  let d = fs.readFileSync(debug ? './changelogdataDebug.json' : './changelogdata.json');
   return JSON.parse(d);
 };
 
-const writeToLog = function(dataPlusChange) {
-  fs.writeFileSync('changelogdata.json', dataPlusChange);
+const writeToLog = function(dataPlusChange, debug = false) {
+  fs.writeFileSync(debug ? 'changelogdataDebug.json' : 'changelogdata.json', dataPlusChange);
 };
 
-const readSuggestionList = function() {
-  let d = fs.readFileSync('./suggestiondata.json');
+const readSuggestionList = function(debug = false) {
+  let d = fs.readFileSync(debug ? './suggestiondataDebug.json' : './suggestiondata.json');
   return JSON.parse(d);
 };
 
-const writeSuggestion = function(dataPlusChange, justTheChange = null) {
-  fs.writeFileSync('suggestiondata.json', dataPlusChange);
+const writeSuggestion = function(dataPlusChange, justTheChange = null, debug = false) {
+  fs.writeFileSync(debug ? './suggestiondataDebug.json' : './suggestiondata.json', dataPlusChange);
   if (justTheChange != null) {
-    fs.appendFileSync('suggestionReadable.txt', `\n\n${justTheChange}`);
+    fs.appendFileSync(debug ? './suggestionReadableDebug.txt' : './suggestionReadable.txt', `\n\n${justTheChange}`);
   }
 };
 
@@ -52,5 +85,6 @@ module.exports = {
   readSuggestionList,
   writeSuggestion,
   formatSuggestion,
-  initializeSuggestionLog
+  initializeSuggestionLog,
+  splitWrapped
 };
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
-const { fieldBuilder, pluckFirstQuotedString, readChangelog, writeToLog, readSuggestionList, writeSuggestion, formatSuggestion, initializeSuggestionLog } = require('./helpers');
+const { fieldBuilder, pluckFirstQuotedString, readChangelog, writeToLog, readSuggestionList, writeSuggestion, formatSuggestion, initializeSuggestionLog, splitWrapped } = require('./helpers');
 var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
 
+// MAKE SURE TO TURN THIS ON WHEN TESTING.
+const debug = true;
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -12,33 +14,34 @@ logger.add(new logger.transports.Console, {
 logger.level = 'debug';
 var bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
-const changelog = readChangelog().contents;
-const suggestions = readSuggestionList().contents;
+const changelog = readChangelog(debug).contents;
+const suggestions = readSuggestionList(debug).contents;
 let changelogEmbed = new Discord.MessageEmbed()
-    .setColor('#6a2aff')
-    .setTitle(`Changelog`)
-    .setURL('https://github.com/Unbidden-Dev-Team/UnbiddenMod')
-    .setAuthor('Unbidden Dev Team', 'https://i.imgur.com/a/nX4113x.png', 'https://github.com/Unbidden-Dev-Team/UnbiddenMod')
-    .setDescription('Mod Changes')
-    .setThumbnail('https://i.imgur.com/a/nX4113x.png')
-    .addFields(...changelog)
-    .setImage('https://i.imgur.com/a/nX4113x.png')
-    .setTimestamp()
-    .setFooter('UnbiddenMod Dev Team', '');
+.setColor('#6a2aff')
+.setTitle(`Changelog`)
+.setURL('https://github.com/Unbidden-Dev-Team/UnbiddenMod')
+.setAuthor('Unbidden Dev Team', 'https://i.imgur.com/a/nX4113x.png', 'https://github.com/Unbidden-Dev-Team/UnbiddenMod')
+.setDescription('Mod Changes')
+.setThumbnail('https://i.imgur.com/a/nX4113x.png')
+.addFields(...changelog)
+.setImage('https://i.imgur.com/a/nX4113x.png')
+.setTimestamp()
+.setFooter('UnbiddenMod Dev Team', '');
 
 let suggestionsEmbed = new Discord.MessageEmbed()
-    .setColor('#6a2aff')
-    .setTitle(`Suggestions`)
-    .setAuthor('Unbidden Dev Team', 'https://i.imgur.com/a/nX4113x.png', 'https://github.com/Unbidden-Dev-Team/UnbiddenMod')
-    .setThumbnail('https://i.imgur.com/a/nX4113x.png')
-    .setTimestamp()
-    .setFooter('UnbiddenMod Dev Team', '');
+.setColor('#6a2aff')
+.setTitle(`Suggestions`)
+.setAuthor('Unbidden Dev Team', 'https://i.imgur.com/a/nX4113x.png', 'https://github.com/Unbidden-Dev-Team/UnbiddenMod')
+.setThumbnail('https://i.imgur.com/a/nX4113x.png')
+.setTimestamp()
+.setFooter('UnbiddenMod Dev Team', '');
 initializeSuggestionLog(suggestions, suggestionsEmbed);
 
 bot.username = 'UnbiddenBot';
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info(`Logged in as: ${bot.user.tag}!`);
+    if (debug) logger.info('DEBUG MODE IS ON.');
 });
 bot.on('message', function (message) {
     const user = message.author,
@@ -49,10 +52,11 @@ bot.on('message', function (message) {
         var args = content.substring(1).split(' ');
         var cmd = args[0];
         args = args.splice(1);
+        var postCommand = args.join(' ');
         switch (cmd) {
             // !ping
             case 'help':
-                message.channel.send("\`\`\`\nWelcome to UnbiddenBot!\nI am currently a Work In Progress, so some commands will not work. If they don't, we will let you know when you try to run it.\n\nOur current commands:\n- !help: Displays this message.\n- !wiki <entry>: Displays the entry provided.\n- !github: Provides a link to the source code, if you're a little curious of the inner machinations.\n- !ihaveabug: Links you to the Issues page of our code, so you can put your issue out there.\n- !changelog: View the changelog, to see what we've done so far.\n- !suggest \"<name>\" \"<idea>\": Formats a suggestion automatically for you. Put down just \"!suggest\" to see the list of suggestions.\`\`\`");
+                message.channel.send("\`\`\`\nWelcome to UnbiddenBot!\nI am currently a Work In Progress, so some commands will not work. If they don't, we will let you know when you try to run it.\n\nOur current commands:\n- !help: Displays this message.\n- !wiki <entry>: Displays the entry provided.\n- !github: Provides a link to the source code, if you're a little curious of the inner machinations.\n- !ihaveabug: Links you to the Issues page of our code, so you can put your issue out there.\n- !changelog: View the changelog, to see what we've done so far.\n- !suggest [<name>] [<idea>]: Formats a suggestion automatically for you. Put down just \"!suggest\" to see the list of suggestions.\nYou must wrap your name and body in [], {}, or (), and both the name and body must have the same wrapper. ex. \"!suggest [ideaName] [ideaBody]\".\`\`\`");
                 break;
             case 'wiki':
                 message.channel.send("Sorry, this command isn't set up yet!");
@@ -70,7 +74,7 @@ bot.on('message', function (message) {
                     if (args[1] !== undefined && args[2] !== undefined) {
                         const field = fieldBuilder(args.splice(1).join(" "), "Done");
                         changelog.push(field);
-                        writeToLog(JSON.stringify({ contents: changelog }, null, 2));
+                        writeToLog(JSON.stringify({ contents: changelog }, null, 2), debug);
 
                         changelogEmbed.addFields(field);
                         message.channel.send("Done!");
@@ -91,17 +95,44 @@ bot.on('message', function (message) {
                         message.channel.send(suggestionsEmbed);
                         break;
                     }
-                    const name = `${pluckFirstQuotedString(args.join(" "))}`;
-                    if (name.length > 40)
+                    let wrappedArgs;
+                    switch (postCommand[0]) {
+                        case '(':
+                            wrappedArgs = splitWrapped(postCommand, '()', false);
+                            break;
+                        case '[':
+                            wrappedArgs = splitWrapped(postCommand, "[]", false);
+                            break;
+                        case '{':
+                            wrappedArgs = splitWrapped(postCommand, "{}", false);
+                            break;
+                        default:
+                            wrappedArgs = null;
+                            break;
+                    }
+                    if (wrappedArgs === null) {
+                        message.channel.send("I think you might have some formatting wrong. Try again!");
+                        break;
+                    }
+                    console.log(wrappedArgs);
+                    const name = wrappedArgs[0];
+                    if (name === undefined)
                     {
+                        message.channel.send("Oops, something went wrong on my end!");
+                        break;
+                    } else if (name.length > 40) {
                         message.channel.send("Your name is fairly long. Make sure it's actually your name, and if it is, concise it!");
                         break;
                     }
-                    const body = content.split('"')[3];
+                    const body = wrappedArgs[1];
+                    if (body === undefined) {
+                        message.channel.send("You're missing a description!");
+                        break;
+                    }
                     const suggestion = formatSuggestion(name, body, message, suggestionsEmbed, suggestions);
                     suggestions.push({ suggestion, id: suggestions.length + 1 });
                     message.channel.send(suggestion);
-                    writeSuggestion(JSON.stringify({ contents: suggestions }), suggestion);
+                    writeSuggestion(JSON.stringify({ contents: suggestions }), suggestion, debug);
                     message.delete();
                 }
                 break;

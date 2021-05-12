96f9bf0f61c22705c05dffd662eaa02e8a561031
Zack <65053215+ZackFox0@users.noreply.github.com>
2021-05-11 21:24:26 -0500

    Suggest works

--- Commands/Suggest.cs
+++ Commands/Suggest.cs
@@ -16,20 +16,28 @@ namespace ProvidenceBot.Commands
     [Description("Suggests something in the #suggestions channel.")]
     public async Task Suggestion(CommandContext context)
     {
-      DiscordMessage commandMesssage = context.Message;
-      DiscordMessage titleQuestion = await context.Channel.SendMessageAsync("What is this suggestion about?");
-      InteractivityExtension titleInteractivity = context.Client.GetInteractivity();
-      InteractivityResult<DiscordMessage> titleResponse = await titleInteractivity.WaitForMessageAsync(x => x.Channel.Name == "suggestions" && x.Channel.Name == context.Channel.Name).ConfigureAwait(false);
-      DiscordMessage titleMessage = titleResponse.Result;
-      DiscordMessage descriptionQuestion = await context.Channel.SendMessageAsync("Please provide a description about this suggestion.");
-      InteractivityExtension descriptionInteractivity = context.Client.GetInteractivity();
-      InteractivityResult<DiscordMessage> descriptionResponse = await descriptionInteractivity.WaitForMessageAsync(x => x.Channel.Name == "suggestions" && x.Channel.Name == context.Channel.Name).ConfigureAwait(false);
-      DiscordMessage descriptionMessage = descriptionResponse.Result;
-      await context.Channel.SendMessageAsync($"{titleMessage} | {context.User.Mention} | {DateTime.Now.Day} - {DateTime.Now.Month} - {DateTime.Now.Year}\n{descriptionMessage}");
-      await titleQuestion.DeleteAsync();
-      await titleMessage.DeleteAsync();
-      await descriptionQuestion.DeleteAsync();
-      await descriptionMessage.DeleteAsync();
+      if (context.Channel.Name != "suggestions")
+      {
+        await context.Channel.SendMessageAsync("Wrong channel! Please go to #suggestions !");
+      }
+      else
+      { 
+        DiscordMessage commandMesssage = context.Message;
+        ulong authorID = context.Message.Author.Id;
+        DiscordMessage titleQuestion = await context.Channel.SendMessageAsync("What is this suggestion about?");
+        InteractivityExtension titleInteractivity = context.Client.GetInteractivity();
+        InteractivityResult<DiscordMessage> titleResponse = await titleInteractivity.WaitForMessageAsync(x => x.Channel.Name == "suggestions" && x.Channel.Name == context.Channel.Name && x.Author.Id == authorID).ConfigureAwait(false);
+        DiscordMessage titleMessage = titleResponse.Result;
+        DiscordMessage descriptionQuestion = await context.Channel.SendMessageAsync("Please provide a description about this suggestion.");
+        InteractivityExtension descriptionInteractivity = context.Client.GetInteractivity();
+        InteractivityResult<DiscordMessage> descriptionResponse = await descriptionInteractivity.WaitForMessageAsync(x => x.Channel.Name == "suggestions" && x.Channel.Name == context.Channel.Name && x.Author.Id == authorID).ConfigureAwait(false);
+        DiscordMessage descriptionMessage = descriptionResponse.Result;
+        await context.Channel.SendMessageAsync($"{titleMessage.Content} | {context.User.Mention} | {DateTime.Now.Day} - {DateTime.Now.Month} - {DateTime.Now.Year}\n{descriptionMessage.Content}");
+        await titleQuestion.DeleteAsync();
+        await titleMessage.DeleteAsync();
+        await descriptionQuestion.DeleteAsync();
+        await descriptionMessage.DeleteAsync();
+      }
     }
   }
 }
